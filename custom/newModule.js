2dadc61fd7e50662e62a96b8f3ada3a8dc949026
pallalarajasekar <pallalarajasekar@gmail.com>
2021-07-03 16:49:21 +0530

    update drive list files

---
+++ custom/newModule.js
@@ -0,0 +1,11 @@
+var myDateTime = function () {
+    return Date();
+  };
+  var add = function(x, y){
+      return x + y;
+  }
+ let massege = "custom module";
+ 
+//module.exports = { myDateTime };
+
+  module.exports = { massege, myDateTime, add } ;
